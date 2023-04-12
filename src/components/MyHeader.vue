2f467e1fd53c44dd05087b4dfcba67978bb3c253
GiuseppeSuanno <suanno.giuseppe@yahoo.it>
2023-04-12 12:23:56 +0200

    Proseguimento con header

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -16,57 +16,128 @@ export default {
 
 <template>
     <div class="container">
-        <div class="row">
-            <div class="col-5">
-                <img class=ms_foto src="../../public/theme_eduprime_logo.png" alt="">
-
-            </div>
-            <div class="col-7 d-flex ">
-                <ul class=" list-unstyled d-flex align-items-center mt-2">
-                    <li>Home<i class="fa-solid fa-caret-down ms-2 me-4"></i></li>
-                    <li>Courses<i class="fa-solid fa-caret-down ms-2 me-4"></i></li>
-                    <li>About Us</li>
-                    <li>News<i class="fa-solid fa-caret-down ms-2 me-4"></i></li>
-                    <li>Pages<i class="fa-solid fa-caret-down ms-2 me-4"></i></li>
-                    <li class="me-4">Contact</li>
-                    <li class="me-4">Purchase</li>
-                </ul>
-                <img class=ms_button src="../../public/hover-button-4.png" alt="Button">
-            </div>
+        <div class="header">
+            <img class=ms_foto src=" ../../public/theme_eduprime_logo.png" alt="">
+            <ul>
+                <li>Home<i class="fa-solid fa-caret-down"></i></li>
+                <li>Courses<i class="fa-solid fa-caret-down"></i></li>
+                <li>About Us</li>
+                <li>News<i class="fa-solid fa-caret-down"></i></li>
+                <li>Pages<i class="fa-solid fa-caret-down"></i></li>
+                <li class="">Contact</li>
+                <li class="">Purchase</li>
+                <button class="btn-yellow">VIEW COURSES</button>
+            </ul>
         </div>
+
+        <div class="text">
+            <h1>Key to your sucess</h1>
+            <p>EduPrime is the most versatile WordPress theme for educational pirposes,showcasing
+                universities,courses,secondary schol etc.</p>
+        </div>
+        <div class="btns">
+            <button class="btn-yellow"><i class="fa-solid fa-magnifying-glass"></i>VIEW COURSES</button>
+            <button class="btn-white"><i class="fa-solid fa-user-plus"></i>Apply for universities</button>
+
+        </div>
+
     </div>
 </template>
 
 
 <style lang="scss">
 @use '../styles/partials/variables.scss';
-@use 'bootstrap';
+
 
 .container {
+
+    //background-image: url(../../public/theme_slider2_bg-1.jpg);
+    //background-size: cover;
     background-color: variables.$bgcolor-salmon;
 
 
+    .header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
 
     .ms_foto {
         height: 50px;
-    }
-
-
-    .ms_button {
-        height: 50px;
-
-
+        margin-left: 30px;
 
     }
 
     ul {
         color: white;
+        display: flex;
+        list-style: none;
+
+    }
+
+    li {
+        margin-left: 15px;
+        margin-top: 10px;
+    }
+
+    i {
+        margin-left: 5px;
     }
 
     li:hover {
-        color: yellow;
+        color: variables.$bgcolor-yellow;
         cursor: pointer;
 
+    }
+
+    .btns {
+        text-align: center;
+        margin-top: 30px;
+    }
+
+    .btn-yellow {
+        background-color: variables.$bgcolor-yellow;
+        border: none;
+        margin-left: 20px;
+        border-radius: 20px;
+        padding: 14px;
+        color: white;
+        font-size: 12px;
+        width: 150px;
+        margin-right: 30px;
+
+        i {
+            margin-right: 10px;
+        }
+    }
+
+
+    .btn-white {
+        background-color: variables.$bgcolor-white;
+        border: none;
+        margin-left: 10px;
+        border-radius: 20px;
+        padding: 14px;
+        color: variables.$bgcolor-salmon;
+        font-size: 12px;
+        width: 170px;
+
+        i {
+            margin-right: 10px;
+        }
+
+
+    }
+
+
+    .text {
+        color: white;
+        text-align: center;
+    }
+
+    .text p {
+        width: 27%;
+        margin: auto;
     }
 
 
